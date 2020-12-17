e8dbc2ce20f8dae0ba91e4d5870f9c7cb3934437
CarsonMcNeff <cjmcneff@outlook.com>
2020-12-17 12:51:25 -0600

    commit tree 84c1c581bebe37b2ec27815a124f7c42b16cbc9c

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,57 @@ class Song
     self.class.all << self
   end
 
+  def self.create 
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.each {|song| return song if song.name == name}
+    nil
+  end
+
+  def self.find_or_create_by_name(name)
+     self.find_by_name(name)? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}.uniq
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
+    info[1].chomp!(".mp3")
+    song = self.new
+    song.name = info[1]
+    song.artist_name = info[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    info = filename.split(" - ")
+    info[1].chomp!(".mp3")
+    song = self.create
+    song.name = info[1]
+    song.artist_name = info[0]
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
